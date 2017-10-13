0e149f62826fbafe375c11302094835156be1dc1
Edward Bai <edwardbai31@gmail.com>
2017-10-13 15:36:34 -0700

    Add p02 hashing

--- hashmap/skeleton/lib/p02_hashing.rb
+++ hashmap/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    my_hash =""
+    self.each { |el| my_hash += el.to_s.to_sym.object_id.hash.to_s }
+    my_hash.to_sym.object_id.hash.to_i
+    # my_hash = 0
+    # self.each_with_index do |el, i|
+    #   my_hash += el.hash * i
+    # end
+    # my_hash
   end
 end
 
 class String
   def hash
+    self.to_sym.object_id.hash
   end
 end
 
@@ -16,6 +25,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort.to_s.to_sym.object_id.hash
   end
 end
