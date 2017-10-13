d906ccd91f54e22ae5ddbb825e1f970c771e60da
Edward Bai <edwardbai31@gmail.com>
2017-10-13 15:44:25 -0700

    Add p03 linked_list

--- hashmap/skeleton/lib/p03_hash_set.rb
+++ hashmap/skeleton/lib/p03_hash_set.rb
@@ -1,7 +1,7 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -9,18 +9,29 @@ class HashSet
   end
 
   def insert(key)
-  end
-
-  def include?(key)
+    unless include?(key)
+      self[key] << key
+      @count += 1
+    end
+    resize! if @count > @store.length
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
+  end
+
+  def include?(key)
+    self[key].include?(key)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](key)
+    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +39,10 @@ class HashSet
   end
 
   def resize!
+    arr = HashSet.new(@store.length * 2)
+    @store.flatten.each do |el|
+      arr.insert(el)
+    end
+    @store = arr.store
   end
 end
